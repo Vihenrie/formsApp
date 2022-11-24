1f1508e3a64e868679a03ba1ce1187c981f8b440
Vinicius Henrique Santos Pinto <viniciushpintolp@gmail.com>
2022-11-24 10:53:34 -0300

    att forms app

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,9 +7,18 @@ import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { IonicStorageModule } from '@ionic/storage-angular';
+import { Drivers } from '@ionic/storage';
+
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
+  imports: [
+    BrowserModule,
+    IonicModule.forRoot(),
+    AppRoutingModule,
+    IonicStorageModule.forRoot({
+    driverOrder: [Drivers.IndexedDB, Drivers.LocalStorage]
+  })],
   providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
   bootstrap: [AppComponent],
 })
---
+++ src/app/services/storage.service.ts
@@ -0,0 +1,44 @@
+/* eslint-disable no-underscore-dangle */
+import { Injectable } from '@angular/core';
+
+import { Storage } from '@ionic/storage-angular';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class StorageService {
+
+  private _storage: Storage | null = null;
+
+  constructor(private storage: Storage) {
+    this.init();
+  }
+
+  async init() {
+    const storage = await this.storage.create();
+    this._storage = storage;
+  }
+
+  // Create and expose methods that users of this service can
+  // call, for example:
+  public set(key: string, value: any) {
+    this._storage?.set(key, value);
+  }
+
+  public get(key: string) {
+    this._storage?.get(key);
+  }
+
+  public remove(key: string) {
+    this._storage?.remove(key);
+  }
+
+  public getAll(){
+    const lista = [];
+    this._storage.forEach((value, key, index) => {
+      lista.push(value);
+    });
+    return lista;
+  }
+
+}
